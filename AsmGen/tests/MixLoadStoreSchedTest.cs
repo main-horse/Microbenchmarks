6e6757517799a62948ce6ac7116137d97761fd51
cheesecake16 <70170443+cheesecake16@users.noreply.github.com>
2024-02-13 20:51:44 -0800

    Add files via upload

--- AsmGen/tests/MixLoadStoreSchedTest.cs
+++ AsmGen/tests/MixLoadStoreSchedTest.cs
@@ -2,66 +2,57 @@
 
 namespace AsmGen
 {
-    public class MixLoadStoreSchedTest : UarchTest
+    public class MixLoadStoreSched : UarchTest
     {
-        public MixLoadStoreSchedTest(int low, int high, int step)
+        public MixLoadStoreSched(int low, int high, int step)
         {
             this.Counts = UarchTestHelpers.GenerateCountArray(low, high, step);
             this.Prefix = "mixloadstoresched";
-            this.Description = "Mixed Load/Store (Address Dependency) scheduler capacity test";
-            this.FunctionDefinitionParameters = "uint64_t iterations, int *arr, int *arr2";
-            this.GetFunctionCallParameters = "structIterations, A, B";
+            this.Description = "Mixed Load/Store Address Scheduler";
+            this.FunctionDefinitionParameters = "uint64_t iterations, int *arr, float *floatArr";
+            this.GetFunctionCallParameters = "structIterations, A, fpArr";
             this.DivideTimeByCount = false;
         }
 
-        public override void GenerateX86GccAsm(StringBuilder sb)
+        public override bool SupportsIsa(IUarchTest.ISA isa)
         {
-            string[] ops1 = new string[4];
-            ops1[0] = "  mov (%rdx, %rdi, 4), %r15";
-            ops1[1] = "  mov %r11, (%r8, %rdi, 4)";
-            ops1[2] = "  mov (%rdx, %rdi, 4), %r13";
-            ops1[3] = "  mov %r11, (%r8, %rdi)";
-
-            string[] ops2 = new string[4];
-            ops2[0] = "  mov (%rdx, %rsi, 4), %r15";
-            ops2[1] = "  mov %r11, (%r8, %rsi, 4)";
-            ops2[2] = "  mov (%rdx, %rsi, 4), %r13";
-            ops2[3] = "  mov %r11, (%r8, %rsi)";
-            UarchTestHelpers.GenerateX86AsmStructureTestFuncs(sb, this.Counts, this.Prefix, ops1, ops2, true);
+            if (isa == IUarchTest.ISA.amd64) return true;
+            if (isa == IUarchTest.ISA.aarch64) return true;
+            return false;
         }
 
-        public override void GenerateX86NasmAsm(StringBuilder sb)
+        public override void GenerateAsm(StringBuilder sb, IUarchTest.ISA isa)
         {
-            // todo
-            string[] dependentLoads = new string[4];
-            dependentLoads[0] = "  mov r15, [r8 + rdi * 4]";
-            dependentLoads[1] = "  mov r14, [r8 + rdi * 4]";
-            dependentLoads[2] = "  mov r13, [r8 + rdi * 4]";
-            dependentLoads[3] = "  mov r12, [r8 + rdi * 4]";
+            if (isa == IUarchTest.ISA.amd64)
+            {
+                string[] dependentLoads = new string[4];
+                dependentLoads[0] = "  mov %r15, (%r8, %rdi, 4)";
+                dependentLoads[1] = "  mov (%r8, %rdi, 2), %r14";
+                dependentLoads[2] = "  mov %r13, (%r8, %rdi, 4)";
+                dependentLoads[3] = "  mov (%r8, %rdi, 2), %r12";
 
-            string[] dependentLoads1 = new string[4];
-            dependentLoads1[0] = "  mov r15, [r8 + rsi * 4]";
-            dependentLoads1[1] = "  mov r14, [r8 + rsi * 4]";
-            dependentLoads1[2] = "  mov r13, [r8 + rsi * 4]";
-            dependentLoads1[3] = "  mov r12, [r8 + rsi * 4]";
-            UarchTestHelpers.GenerateX86NasmStructureTestFuncs(sb, this.Counts, this.Prefix, dependentLoads, dependentLoads1, false);
-        }
+                string[] dependentLoads1 = new string[4];
+                dependentLoads1[0] = "  mov %r15, (%r8, %rsi, 4)";
+                dependentLoads1[1] = "  mov (%r8, %rsi, 4), %r14";
+                dependentLoads1[2] = "  mov %r13, (%r8, %rsi, 4)";
+                dependentLoads1[3] = "  mov (%r8, %rsi, 4), %r12";
+                UarchTestHelpers.GenerateX86AsmStructureTestFuncs(sb, this.Counts, this.Prefix, dependentLoads, dependentLoads1, includePtrChasingLoads: true);
+            }
+            else if (isa == IUarchTest.ISA.aarch64)
+            {
+                string[] dependentLoads = new string[4];
+                dependentLoads[0] = "  str w15, [x2, w25, uxtw #2]";
+                dependentLoads[1] = "  ldr w14, [x1, w25, uxtw #0]";
+                dependentLoads[2] = "  str w13, [x2, w25, uxtw #2]";
+                dependentLoads[3] = "  ldr w12, [x1, w25, uxtw #0]";
 
-        public override void GenerateArmAsm(StringBuilder sb)
-        {
-            // todo
-            string[] dependentLoads = new string[4];
-            dependentLoads[0] = "  ldr w15, [x2, w25, uxtw #2]";
-            dependentLoads[1] = "  ldr w14, [x2, w25, uxtw #2]";
-            dependentLoads[2] = "  ldr w13, [x2, w25, uxtw #2]";
-            dependentLoads[3] = "  ldr w12, [x2, w25, uxtw #2]";
-
-            string[] dependentLoads1 = new string[4];
-            dependentLoads1[0] = "  ldr w15, [x2, w26, uxtw #2]";
-            dependentLoads1[1] = "  ldr w14, [x2, w26, uxtw #2]";
-            dependentLoads1[2] = "  ldr w13, [x2, w26, uxtw #2]";
-            dependentLoads1[3] = "  ldr w12, [x2, w26, uxtw #2]";
-            UarchTestHelpers.GenerateArmAsmStructureTestFuncs(sb, this.Counts, this.Prefix, dependentLoads, dependentLoads1, false);
+                string[] dependentLoads1 = new string[4];
+                dependentLoads1[0] = "  str w15, [x2, w26, uxtw #2]";
+                dependentLoads1[1] = "  ldr w14, [x1, w26, uxtw #0]";
+                dependentLoads1[2] = "  str w13, [x2, w26, uxtw #2]";
+                dependentLoads1[3] = "  ldr w12, [x1, w26, uxtw #0]";
+                UarchTestHelpers.GenerateArmAsmStructureTestFuncs(sb, this.Counts, this.Prefix, dependentLoads, dependentLoads1, includePtrChasingLoads: true);
+            }
         }
     }
 }
